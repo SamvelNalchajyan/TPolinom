7e55d498848f247aa82c5c89b39672a89438b79d
Makarov-AA <43129398+Makarov-AA@users.noreply.github.com>
2020-02-15 15:36:33 +0300

    First Commit

---
+++ TList/TList/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstdlib>
+#include <clocale>
+#include "TPolinom.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	/*..........................................*/
+
+	/*..........................................*/
+	system("pause");
+	return 0;
+}
